445bc01ea564d7d495d84ad6776825fb8172beab
kellyloria <kelo2727@colorado.edu>
2020-07-26 18:06:43 -0700

    Initial script set up with file paths.

---
+++ StreamMetabolizerPractice.R
@@ -0,0 +1,19 @@
+## ---------------------------
+## Practice space for stream metabolizer package used in Appling et al. 2018
+##
+## Author: Kelly A. Loria
+## Date Created: 2020-07-26
+## Email: kelly.loria@nevada.unr.edu
+##
+## ---------------------------
+## Load packages:
+library(StreamMetabolism)
+
+## ---------------------------
+# File path setup:
+if (dir.exists('/Users/kellyloria/Documents/UNR_2020/Fall2020Projects')){
+  inputDir<- '/Users/kellyloria/Documents/UNR_2020/Fall2020Projects/StreamMetablismPractice_Data'
+  outputDir<- '/Users/kellyloria/Documents/UNR_2020/Fall2020Projects/StreamMetablismPractice_Output' 
+}
+
+## ---------------------------
